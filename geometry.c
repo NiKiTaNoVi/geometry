0b7c4b2b35944548fbd011365baec977e33f8afd
NiKiTaNoVi <nikitka.novikov.2001@list.ru>
2019-03-13 10:23:38 +0700

    Wrote a program to enter the number of laps

--- geometry.c
+++ geometry.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
+int Circle(int x, int y, int z)
+{
+	printf("Circle (%d, %d, %d)\n",x, y, z);
+}
 
 main()
 {
-	printf("Hello World");
+	int n, *a_x, *a_y, *a_z;
+	printf("Enter the number of circles ");
+	scanf("%d",&n);
+
+	
+	
 	return 0;
 }
 
