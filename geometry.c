e62c86066be1ff9ce7358195bc6b54fbba7ed6c8
NiKiTaNoVi <nikitka.novikov.2001@list.ru>
2019-03-20 11:01:14 +0700

    Circle input and output

--- geometry.c
+++ geometry.c
@@ -1,19 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int Circle(int x, int y, int z)
+void Circle(int x, int y, int r)
 {
-	printf("Circle (%d, %d, %d)\n",x, y, z);
+	printf("Circle (%d, %d, %d)\n",x, y, r);
 }
 
-main()
+int main()
 {
-	int n, *a_x, *a_y, *a_z;
+	int n, i, *A_x, *A_y, *A_r;
 	printf("Enter the number of circles ");
 	scanf("%d",&n);
+	A_x = (int*)malloc(n * sizeof(int));
+	A_y = (int*)malloc(n * sizeof(int));
+	A_r = (int*)malloc(n * sizeof(int));
 
+for (i = 0; i < n; i++){
+	printf("Enter x, y, and the radius of the circle %d ", i + 1);
+	scanf("%d%d%d", &A_x[i], &A_y[i], &A_r[i]);
 	
-	
+}
+
+for (i = 0; i < n; i++){
+	printf("%d ", i + 1);
+	Circle(A_x[i], A_y[i], A_r[i]);
+}
+
+
 	return 0;
 }
+
 
